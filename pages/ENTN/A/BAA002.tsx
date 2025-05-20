9e05ded852abae7519b8b1558045f9db66459acf
hermes7144 <kichancho@gmail.com>
2025-05-20 14:06:48 +0900

    feat:wip

--- pages/ENTN/A/BAA002.tsx
+++ pages/ENTN/A/BAA002.tsx
@@ -3,34 +3,29 @@
 import { useProcedure } from '@/app/hooks/useProcedure';
 import { useEffect, useState } from 'react';
 
-interface ScheduleItem {
-  mojib_gb: string;
-  sebu_iljeong_gb: string;
-  mojib_fr_dt: string;
-  mojib_to_dt: string;
-  jageob_yn: string;
-}  
 
 export default function BAA002Page() {
-  const [data, setData] = useState<ScheduleItem[]>([]);
+  const [data, setData] = useState();
   const { callProcedure } = useProcedure();
 
   useEffect(() => {
-    callProcedure<ScheduleItem[]>('post', '조회', {
+    callProcedure('post', '조회', {
       mojib_yy: '2024',
       ibhag_gb: 'B35001',
-      mojib_gb: 'B01001',
-    }).then(setData).catch(console.error);
+      mojib_gb: 'NONE',
+      program_id:"BAA002",
+      sabeon:'360852',
+    }).then(res => setData(res[0])).catch(console.error);
   }, [callProcedure]);
 
-  const handleClick = async () => {
+  const handleClick = async (flag) => {
     try {
       await callProcedure('put', '확정처리', {
         mojib_yy: '2024',
         ibhag_gb: 'B35001',
         mojib_gb: 'NONE',
         program_id: 'BAA002',
-        hwagjeong_yn: 'N',
+        hwagjeong_yn: flag,
         id: '360852',
         ip: '127.0.0.1',
       });
@@ -41,17 +36,85 @@ export default function BAA002Page() {
   };
 
   return (
-    <div>
-      <button onClick={handleClick}>확정</button>
-      {data.map(item => (
-        <div key={item.sebu_iljeong_gb}>
-          <div>{item.mojib_gb}</div>
-          <div>{item.sebu_iljeong_gb}</div>
-          <div>{item.mojib_fr_dt}</div>
-          <div>{item.mojib_to_dt}</div>
-          <div>{item.jageob_yn}</div>
-        </div>
-      ))}
+  <div className="filters">
+    {data && <>
+          <div className="filters__wrap" style={{ width: '70%' }}>
+        <table className="TableStyle01" style={{ margin: 0 }} width="100%" cellSpacing="0" cellPadding="0">
+          <caption>확정처리 폼입니다.</caption>
+          <colgroup>
+            <col width="10%" />
+            <col width="17%" />
+            <col width="10%" />
+            <col width="10%" />
+            <col width="10%" />
+            <col width="17%" />
+            <col width="10%" />
+            <col width="10%" />
+          </colgroup>
+          <tbody>
+            <tr>
+              <th className="textWrap Center">
+                <label htmlFor="firstConfirmedAt">최초확정일시</label>
+              </th>
+              <td className="inputWrap Center">
+                <span id="firstConfirmedAt">{data?.hwagjeong_1st_dt}</span>
+              </td>
+
+              <th className="textWrap Center">
+                <label htmlFor="firstConfirmer">최초확정자</label>
+              </th>
+              <td className="inputWrap Center">
+                <strong id="firstConfirmer">{data?.hwagjeongja}</strong>
+              </td>
+
+              <th className="textWrap Center">
+                <label htmlFor="reConfirmedAt">
+                  {data?.hwagjeong_yn ? '재확정일시': '확정취소일시'}
+                </label>
+              </th>
+              <td className="inputWrap Center">
+                <span id="reConfirmedAt">{data?.hwagjeong_dt}</span>
+              </td>
+
+              <th className="textWrap Center">
+                <label htmlFor="confirmer">
+                  {data?.hwagjeong_yn ? '확정처리자' : '확정취소자'}
+                </label>
+              </th>
+              <td className="inputWrap Center">
+                <strong id="confirmer">{data?.hwagjeongja}</strong>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div className="filters__btnArea" id="hwagjeongBtnArea">
+       {data.hwagjeong_yn  === 'Y' ? <button
+          type="button"
+          className="bem-btn bem-btn--confirm bem-icon--check"
+          onClick={() => {
+            handleClick('N');
+          }}
+        >
+          확정취소
+        </button> :
+                <button
+          type="button"
+          className="bem-btn bem-btn--revert bem-icon--revert"
+          onClick={() => {
+            handleClick('Y');
+          }}
+        >
+          확정
+        </button>
+      } 
+
+      </div>
+    
+    </>
+    
+    }
     </div>
   );
 }
--- src/app/api/ENTN/A/BAA002/route.ts
+++ src/app/api/ENTN/A/BAA002/route.ts
@@ -2,7 +2,7 @@ import { handleProcedureRequest } from '@/app/lib/procedure.util';
 import { NextRequest } from 'next/server';
 
 const procedureMap: Record<string, string> = {
-  조회: 'uwindb1.입학행정_입시관리학부_기준정보관리_일정목록',
+  조회: 'uwindb1.입학행정_입시공통_기준정보확정관리_확정상태',
   확정처리: 'uwindb1.입학행정_입시공통_기준정보확정관리_확정처리',
 };
 
